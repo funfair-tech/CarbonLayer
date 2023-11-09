477b782d1d774d4863290682c66d0f21e729d437
Dane Wiid <dane@funfair.io>
2023-11-09 16:41:18 +0000

    feat: set initial hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.6",
+      },
+      {
+        version: "0.8.7",
+        settings: {},
+      },
+    ],
+  },
 };
 
 export default config;
