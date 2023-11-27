b4f396005d96e75e08e3c4979a9610f9f4ea6fe8
Dane Wiid <dane@funfair.io>
2023-11-27 21:56:06 +0000

    feat: adds sedn and fulfill sample

---
+++ examples/externalFunctionCall/lambda.js
@@ -0,0 +1,34 @@
+const { GPU } = require('gpu.js');
+
+exports.handler = async (event) => {
+    const startTime = Date.now();
+
+    // Simulate GPU-like rendering with gpu.js
+    const gpu = new GPU();
+    const renderKernel = gpu.createKernel(function() {
+        // Simulate a GPU-intensive task
+        let value = 0;
+        for (let i = 0; i < 100000; i++) {
+            value += Math.sin(i) * Math.cos(i);
+        }
+        return value;
+    }).setOutput([1]);
+
+    const result = renderKernel();
+
+    const endTime = Date.now();
+    const duration = endTime - startTime;
+
+    console.log(`Duration: ${duration} ms`);
+    console.log(`GPU Simulation Result: ${result[0]}`);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'GPU rendering simulation completed!',
+            duration: duration,
+            result: result[0],
+        }),
+    };
+    return response;
+};
