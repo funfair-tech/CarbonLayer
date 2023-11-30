be43cedb8604e104416e9137218a6eb07b4ca275
Peter Heywood <peter.heywood@funfair.io>
2023-11-30 10:27:07 +0000

    feat: Tidy CarbonCompute, set apiResonse to whole returned object

--- examples/carbonCompute/CarbonCompute.sol
+++ examples/carbonCompute/CarbonCompute.sol
@@ -11,20 +11,27 @@ import { FunctionsRequest } from "@chainlink/contracts/src/v0.8/functions/dev/v1
 /**
  * @title FunctionClient
  * @notice This is an example contract to show how to make HTTP requests using CarbonLayer
+ *      and charge a fee dependent on the current state of energy production
  */
 contract CarbonCompute is FunctionsClient, ConfirmedOwner {
     using FunctionsRequest for FunctionsRequest.Request;
 
-    // State variables to store the last request ID, response, and error
-    bytes32 public s_lastRequestId;
-    bytes public s_lastResponse;
-    bytes public s_lastError;
 
-    // State variables to store the fees and energy source threshold
+    // State variables to store the fees and energy source threshold where the reduced fee is charged
     uint256 public standardFee = 0.002 ether;
     uint256 public reducedFee = 0.001 ether;
-    uint16 public threshold = 3000;
-    CarbonQuery public carbonQueryInstance;
+    uint16 public threshold = 3000; // Above this is reduced fees are charged (TODO! unit?)
+    CarbonQuery public carbonQueryInstance; // The contract to call to get the current carbon index
+ 
+    // State variables to store the last request ID, response, and error returned from the lambda function call
+    bytes32 public lastRequestId;
+    bytes public lastResponse;
+    bytes public lastError;
+        
+    uint32 callbackGasLimit = 300000;
+
+    // State variable to store the returned apicall information
+    string public apiResponse;
 
     // Errors
     error UnexpectedRequestID(bytes32 requestId);
@@ -36,7 +43,7 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
     event Withdraw(string indexed _msg, uint256 indexed fee, address indexed recipient);
     event Response(
         bytes32 indexed requestId,
-        string character,
+        string apiResonse,
         bytes response,
         bytes err
     );
@@ -46,35 +53,19 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
     address router = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
     bytes32 donID = 0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
 
-    // JavaScript source code
-    // Trigger an AWS lambda function.
-    // Documentation: https://aws.amazon.com/lambda/resources/?aws-lambda-resources-blog.sort-by=item.additionalFields.createdDate&aws-lambda-resources-blog.sort-order=desc
-    string source =
-        "const characterId = args[0];"
-        "const apiResponse = await Functions.makeHttpRequest({"
-        "url: `https://v2nnosvq76yihfm2mxk7bvep2q0jtnrl.lambda-url.eu-west-2.on.aws/`"
-        "});"
-        "if (apiResponse.error) {"
-        "throw Error('Request failed');"
-        "}"
-        "const { data } = apiResponse;"
-        "return Functions.encodeString(data.name);";
 
-    //Callback gas limit
-    uint32 gasLimit = 300000;
-
-    // State variable to store the returned character information
-    string public character;
 
     /**
      * @notice Initializes the contract with the Chainlink router address and sets the contract owner
+     * @param _carbonQueryAddress The address of the contract to call to get the current carbon index 
      */
     constructor(address _carbonQueryAddress) FunctionsClient(router) ConfirmedOwner(msg.sender) {
         setCarbonQuery(_carbonQueryAddress);
     }
 
     /**
-     * @notice Admin functions to control the fees
+     * @notice Update the retained address of the carbon index contract
+     * @param _carbonQueryAddress The address of the contract to call to get the current carbon index 
      */
     function setCarbonQuery(address _carbonQueryAddress) public onlyOwner {
         require(_carbonQueryAddress != address(0), 'Invalid CarbonQuery address');
@@ -84,16 +75,27 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
         emit CarbonQueryInstanceSet(_carbonQueryAddress);
     }
 
-
+   /**
+     * @notice Update the fees charged for calling doWork
+     * @param _standardFee Setter fof the fee chanrged when the index is below the threshold
+     * @param _reducedFee Setter for the fee charged when the index is above the threshold
+     */
     function setFees(uint256 _standardFee, uint256 _reducedFee) external onlyOwner {
         standardFee = _standardFee;
         reducedFee = _reducedFee;
     }
 
+   /**
+     * @notice Update the threshold at which reduced fees are charged
+     * @param _threshold Setter for the resuced charges threshold
+     */
     function setTreshold(uint16 _threshold) external onlyOwner {
         threshold = _threshold;
     }
 
+   /**
+     * @notice Whith draw all the accumulated fees to the owners address
+     */
     function withdraw() external onlyOwner {
         uint256 balance = address(this).balance;
 
@@ -104,28 +106,35 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
         emit Withdraw("Fee withdraw processed", balance, owner());
     }
 
+   /**
+     * @notice The main public entry point to the contract
+     * @param _subscriptionID The Chainlink link subscription to use
+     * @param _args arguments to be passed to the called lambda funtion
+     */
     function doWork(uint64 _subscriptionID, string[] calldata _args) external payable {
         
+        // Get the fee we're going to charge for this work       
         uint256 fee = quote();
 
         require(msg.value >= fee, "Insufficient fee attached");
 
+        // Call the lambda function 
         sendRequest(_subscriptionID, _args);
         
-        emit Working("Demo01 is doing work for a fee of ", fee);
+        emit Working("CarbonCompute doing work for a fee of ", fee);
 
         // Transfer the excess funds back to the caller
         if (msg.value > fee) {
-            // In Solidity versions 0.8.0 and later, 
-            // the SafeMath library functions are part of the standard arithmetic operations for uint256.
             uint256 refund = msg.value - fee;
             payable(msg.sender).transfer(refund);
 
             emit Refunding("Refunding exess fee to caller", refund);
-
         }
     }
 
+   /**
+     * @notice Call the carbonQuery contract to establish the fees to charge for this work
+     */
     function quote() public view returns (uint256) {
         bool isCarbonNeutral = carbonQueryInstance.carbonNeutralPowered(threshold);
         
@@ -134,8 +143,24 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
         return fee;
     }
 
+
+    // JavaScript source code to be executed as a Chainlink function
+    // Call an AWS lambda function and return the result
+    // Documentation: https://aws.amazon.com/lambda/resources/?aws-lambda-resources-blog.sort-by=item.additionalFields.createdDate&aws-lambda-resources-blog.sort-order=desc
+    string source =
+        "const characterId = args[0];"
+        "const apiResponse = await Functions.makeHttpRequest({"
+        "url: `https://v2nnosvq76yihfm2mxk7bvep2q0jtnrl.lambda-url.eu-west-2.on.aws/`"
+        "});"
+        "if (apiResponse.error) {"
+        "throw Error('Request failed');"
+        "}"
+        "const { data } = apiResponse;"
+        "return Functions.encodeString(JSON.stringify(data));";
+
+
     /**
-     * @notice Sends an HTTP request for character information
+     * @notice Sends an HTTP request
      * @param subscriptionId The ID for the Chainlink subscription
      * @param args The arguments to pass to the HTTP request
      * @return requestId The ID of the request
@@ -149,14 +174,14 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
         if (args.length > 0) req.setArgs(args); // Set the arguments for the request
 
         // Send the request and store the request ID
-        s_lastRequestId = _sendRequest(
+        lastRequestId = _sendRequest(
             req.encodeCBOR(),
             subscriptionId,
-            gasLimit,
+            callbackGasLimit,
             donID
         );
 
-        return s_lastRequestId;
+        return lastRequestId;
     }
 
     /**
@@ -170,15 +195,15 @@ contract CarbonCompute is FunctionsClient, ConfirmedOwner {
         bytes memory response,
         bytes memory err
     ) internal override {
-        if (s_lastRequestId != requestId) {
+        if (lastRequestId != requestId) {
             revert UnexpectedRequestID(requestId); // Check if request IDs match
         }
         // Update the contract's state variables with the response and any errors
-        s_lastResponse = response;
-        character = string(response);
-        s_lastError = err;
+        lastResponse = response;
+        apiResponse = string(response);
+        lastError = err;
 
         // Emit an event to log the response
-        emit Response(requestId, character, s_lastResponse, s_lastError);
+        emit Response(requestId, apiResponse, lastResponse, lastError);
     }
 }
